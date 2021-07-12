01130645e4d12cf969cbd873375ac39b90c3ba20
dloopes <dl@loopes.com.br>
2021-07-12 12:00:03 -0300

    Corrige caminho do upload

--- api/routes/uploads.js
+++ api/routes/uploads.js
@@ -11,7 +11,7 @@ const createPath = (path) => {
   !fs.existsSync(path) && fs.mkdirSync(path)
 }
 
-const UPLOAD_PATH = 'api/uploads/'
+const UPLOAD_PATH = '/api/uploads/'
 createPath(UPLOAD_PATH)
 
 const imagesPath = () => {
@@ -101,7 +101,7 @@ router.post(
 
 const documentsPath = () => {
   let path = UPLOAD_PATH
-  path += '/documents/'
+  path += 'documents/'
   createPath(path)
   return path
 }
