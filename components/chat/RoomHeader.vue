2da7f06f47b1c84ab89226c6c4708678c9261f39
vractal <v@fractopia.org>
2021-08-12 11:52:35 -0300

    Gambi pra mostrar placeholder qd a sala vier sem

--- components/chat/RoomHeader.vue
+++ components/chat/RoomHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="d-flex flex-column justify-content-between chat-header">
-    <h2>{{ roomName }}</h2>
+    <h2>{{ roomName || 'Nome da Sala' }}</h2>
     <div class="button align-self-end">
       <b-button @click="onCallClick">
         <b-icon icon="camera-video-fill"></b-icon> VideoChamada
@@ -10,7 +10,7 @@
 </template>
 <script>
 export default {
-  props: { roomName: { type: String, default: () => 'Sala' } },
+  props: { roomName: { type: String, default: () => 'Nome da Sala' } },
   methods: {
     onCallClick() {
       this.$store.commit('startVideoCall')
