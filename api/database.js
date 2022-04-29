9ed83eb6892f54a8887977b4d9b3b343bb7fab1e
debinho <debinho@debian-BULLSEYE-live-builder-AMD64>
2022-04-29 12:00:46 -0400

    Adiciona lógica para mongo-db remoto

--- api/database.js
+++ api/database.js
@@ -1,5 +1,5 @@
+import fs from 'fs'
 import mongoose from 'mongoose'
-
 import './models/Note'
 import './models/Project'
 import './models/Card'
@@ -10,6 +10,9 @@ import './models/Organization'
 const isProduction = process.env.NODE_ENV === 'production'
 
 if (isProduction) {
+  // Create certificate file from env var (because mongoose doesnt accept cert data directly)
+  fs.writeFileSync('./api/config/mongodb.ca.crt', process.env.MONGO_CA_CERT)
+
   mongoose.connect(process.env.MONGODB_URI, {
     keepAlive: 1,
     connectTimeoutMS: 30000,
